03e6c581cab65f2940538057ebfc3cfc611c64de
Karsten Zhou <54686209+XiaoSong-CPE@users.noreply.github.com>
2023-04-18 11:15:08 +0800

    Update index.vue

--- src/views/chat/layout/sider/index.vue
+++ src/views/chat/layout/sider/index.vue
@@ -79,7 +79,7 @@ watch(
         <div class="flex-1 min-h-0 pb-4 overflow-hidden">
           <List />
         </div>
-        <div class="p-4">
+        <div class="p-4" v-if="false">
           <NButton block @click="show = true">
             {{ $t('store.siderButton') }}
           </NButton>
