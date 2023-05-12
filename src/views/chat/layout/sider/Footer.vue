4212b5b01a58019a581df693c609cd26005382af
Karsten Zhou <1339688323@qq.com>
2023-05-13 02:35:19 +0800

    Update Footer.vue

--- src/views/chat/layout/sider/Footer.vue
+++ src/views/chat/layout/sider/Footer.vue
@@ -1,10 +1,5 @@
 <script setup lang='ts'>
-import { defineAsyncComponent, ref } from 'vue'
-import { HoverButton, SvgIcon, UserAvatar } from '@/components/common'
-
-const Setting = defineAsyncComponent(() => import('@/components/common/Setting/index.vue'))
-
-const show = ref(false)
+import { UserAvatar } from '@/components/common'
 </script>
 
 <template>
@@ -12,7 +7,7 @@ const show = ref(false)
     <div class="flex-1 flex-shrink-0 overflow-hidden">
       <UserAvatar />
     </div>
-<!-- 
+  <!--
     <HoverButton @click="show = true">
       <span class="text-xl text-[#4f555e] dark:text-white">
         <SvgIcon icon="ri:settings-4-line" />
